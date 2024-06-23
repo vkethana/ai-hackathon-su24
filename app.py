7b877188b337dabc9e8c49a65fb53c60bca19693
Vijay Kethanaboyina <vijaykethanaboyina@gmail.com>
2024-06-22 19:40:42 -0700

    added some multiplayer ifnrastructure but not working yet

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request, jsonify
 from functions import generate_ai_response, evaluate_translation, get_ethical_question
 import time
 import random
+from flask_socketio import SocketIO, join_room, leave_room, emit
+import uuid
 
 app = Flask(__name__)
 DISABLE_API_CALLS = True
+
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+games = {}
 
 @app.route('/')
 def index():
@@ -47,6 +53,50 @@ def compare_responses():
         winner = 'error'
     return jsonify({'winner': winner})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/create_multiplayer_game', methods=['POST'])
+def create_multiplayer_game():
+    print("Creating Multiplayer Game")
+    game_id = str(uuid.uuid4())
+    games[game_id] = {
+        'players': [],
+        'responses': {},
+        'question': get_ethical_question()
+    }
+    return jsonify({'game_id': game_id})
 
+@socketio.on('join')
+def on_join(data):
+    print("Joining Multiplayer Game")
+    game_id = data['game_id']
+    if game_id in games and len(games[game_id]['players']) < 2:
+        print("Game_id", game_id, "exists!")
+        print("Total games", games)
+
+        join_room(game_id)
+        games[game_id]['players'].append(request.sid)
+        print("Players in game", games[game_id]['players'])
+        if len(games[game_id]['players']) == 2:
+            emit('start_game', {'question': games[game_id]['question']}, room=game_id)
+
+@socketio.on('submit_response')
+def on_submit_response(data):
+    game_id = data['game_id']
+    response = data['response']
+    games[game_id]['responses'][request.sid] = response
+
+    if len(games[game_id]['responses']) == 2:
+        # Both players have submitted, get AI response and compare
+        ai_response = generate_ai_response(games[game_id]['question'])
+        results = {}
+        for player, resp in games[game_id]['responses'].items():
+            result = evaluate_translation(resp, ai_response, games[game_id]['question'])
+            results[player] = 'win' if result == 1 else 'lose'
+        emit('game_result', {
+            'results': results,
+            'ai_response': ai_response
+        }, room=game_id)
+        # Clean up the game
+        del games[game_id]
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
